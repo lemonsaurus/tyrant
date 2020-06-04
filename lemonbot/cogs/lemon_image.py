2ab8fe758b1c2af2ddb4fe7fa5d36b1f82318a47
Leon Sandøy <leon.sandoy@conmodo.com>
2020-06-05 01:25:50 +0200

    Move asset ID to asset init

--- lemonbot/cogs/lemon_image.py
+++ lemonbot/cogs/lemon_image.py
@@ -1,11 +1,10 @@
-import itertools
 import logging
-import random
-from pathlib import Path
 
 from discord import File
 from discord.ext import commands
 from discord.ext.commands import Bot, Cog, Context
+
+from lemonbot.assets import FILE_CYCLE
 
 log = logging.getLogger(__name__)
 
@@ -16,16 +15,11 @@ class LemonImage(Cog):
     def __init__(self, bot: Bot):
         """Initialize this cog with the Bot instance."""
         self.bot = bot
-        self.assets = (Path(__file__).parent.parent / "assets")
-
-        shuffled = [f for f in self.assets.iterdir()]
-        random.shuffle(shuffled)
-        self.files = itertools.cycle(shuffled)
 
     @commands.command()
     async def lemon(self, ctx: Context):
         """Post a random image of a lemon."""
-        random_lemon = File(next(self.files))
+        random_lemon = File(next(FILE_CYCLE))
         await ctx.send(file=random_lemon)
 
 
