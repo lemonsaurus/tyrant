d9424e3cf2a330a988865f8e912097bf499a5fd3
Leon Sandøy <leon.sandoy@conmodo.com>
2020-06-05 01:33:45 +0200

    oops, can't attach the pyfile itself.

--- lemonbot/assets/__init__.py
+++ lemonbot/assets/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # Locate and shuffle files
 _assets = Path(__file__).parent
-_shuffled = [f for f in _assets.iterdir()]
+_shuffled = [f for f in _assets.iterdir() if not f.suffix == ".py"]
 random.shuffle(_shuffled)
 
 # Public variables
