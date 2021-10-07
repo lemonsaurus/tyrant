2244b7aa36591b2f962175180ff437fa062309a0
Chris Lovering <chris.lovering.95@gmail.com>
2021-10-07 16:46:49 +0100

    Create and use a single http_session on init
    This creates a TCPConnector on login with an ASync DNS resolver.
    
    This connector is then passed to the super() to be used by discord.py's internals, as well as used for creating a http_session bot attr for use in our cogs.

--- tyrant/bot.py
+++ tyrant/bot.py
@@ -1,6 +1,9 @@
 import logging
+import socket
+from typing import Optional
 
 import discord
+from aiohttp import AsyncResolver, ClientSession, TCPConnector
 from discord.ext import commands
 
 from tyrant import constants
@@ -13,9 +16,39 @@ class Tyrant(commands.Bot):
 
     name = "Tyrant"
 
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.http_session: Optional[ClientSession] = None
+        self._connector: Optional[TCPConnector] = None
+
+    async def login(self, *args, **kwargs) -> None:
+        """Re-create the connector and set up sessions before logging into Discord."""
+        # Use asyncio for DNS resolution instead of threads so threads aren't spammed.
+        self._connector = TCPConnector(
+            resolver=AsyncResolver(),
+            family=socket.AF_INET,
+        )
+
+        # super() will use this connection for it's internal session.
+        self.http.connector = self._connector
+
+        self.http_session = ClientSession(connector=self._connector)
+
+        await super().login(*args, **kwargs)
+
+    async def close(self) -> None:
+        """Close http session when bot is shutting down."""
+        await super().close()
+
+        if self.http_session:
+            await self.http_session.close()
+        if self._connector:
+            await self._connector.close()
+
     async def on_ready(self) -> None:
         await self.check_channels()
-        await self.send_log("Connected!")
+        await self.send_connection_log()
 
     def add_cog(self, cog: commands.Cog) -> None:
         """
@@ -39,7 +72,7 @@ class Tyrant(commands.Bot):
             if channel_id not in all_channels_ids:
                 log.error(f'Channel "{name}" with ID {channel_id} missing')
 
-    async def send_log(self, details: str) -> None:
+    async def send_connection_log(self) -> None:
         """Send a message to the logs channel."""
         try:
             webhook = await self.fetch_webhook(constants.Webhooks.logs)
